1737854e42468c0c0fb88fcc6b8a928b753986fc
fsanntiago <zerofabricio@hotmail.com>
2022-05-20 20:53:41 -0300

    feat(Dashboard): Add HighlightCards

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -59,3 +59,13 @@ export const Icon = styled(Ionicons)`
   color: ${({ theme }) => theme.colors.secondary};
   font-size: ${RFValue(24)}px;
 `;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 }
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(24)}px;
+`;
