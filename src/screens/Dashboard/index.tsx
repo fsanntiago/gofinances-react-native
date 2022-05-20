b493dbc4695b2f9a7da588ca48884a04499de972
fsanntiago <zerofabricio@hotmail.com>
2022-05-20 16:47:19 -0300

    feat(dashboard): Add user info

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/85093481?v=4"
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Fabricio</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    </Container>
+  );
+}
