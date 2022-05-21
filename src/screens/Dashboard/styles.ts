dd8355fd4cb2ac22b824b40f17a22b7bb82b7586
fsanntiago <zerofabricio@hotmail.com>
2022-05-20 21:03:33 -0300

    feat(Dashboard): Add TransactionsCards

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,13 @@
 import styled from "styled-components/native";
+import { FlatList } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import {
+  getBottomSpace,
+  getStatusBarHeight
+} from "react-native-iphone-x-helper";
+
+import { DataListProps } from ".";
 
 export const Container = styled.View`
   flex: 1;
@@ -10,18 +17,15 @@ export const Container = styled.View`
 export const Header = styled.View`
   width: 100%;
   height: ${RFPercentage(42)}px;
-
   background-color: ${({ theme }) => theme.colors.primary};
-
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
 `;
 
 export const UserWrapper = styled.View`
   width: 100%;
-
   padding: 0 24px;
-
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -35,7 +39,6 @@ export const UserInfo = styled.View`
 export const Photo = styled.Image`
   width: ${RFValue(48)}px;
   height: ${RFValue(48)}px;
-
   border-radius: 10px;
 `;
 
@@ -69,3 +72,23 @@ export const HighlightCards = styled.ScrollView.attrs({
   position: absolute;
   margin-top: ${RFPercentage(24)}px;
 `;
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  margin-top: ${RFPercentage(18)}px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.text_dark};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  margin-bottom: ${RFValue(16)}px;
+`;
+
+export const TransactionList = styled(
+  FlatList as new () => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { paddingBottom: getBottomSpace() }
+})``;
