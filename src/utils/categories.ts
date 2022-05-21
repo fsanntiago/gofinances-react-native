921368a282d271ca5ae3a8234c100f0eea4ee6e9
fsanntiago <zerofabricio@hotmail.com>
2022-05-20 21:52:08 -0300

    Add utils

---
+++ src/utils/categories.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    key: "purchases",
+    name: "Compras",
+    icon: "basket-outline",
+    color: "#5636D3"
+  },
+  {
+    key: "food",
+    name: "Alimentação",
+    icon: "fast-food-outline",
+    color: "#FF872C"
+  },
+  { key: "salary", name: "Salário", icon: "cash-outline", color: "#12A454" },
+  { key: "car", name: "Carro", icon: "car-outline", color: "#E83F5B" },
+  { key: "leisure", name: "Lazer", icon: "heart-outline", color: "#26195C" },
+  { key: "studies", name: "Estudos", icon: "school-outline", color: "#9C001A" }
+];
