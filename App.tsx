3e8abf5ae2a98671875176b6861fbe1d6b5c0a84
fsanntiago <zerofabricio@hotmail.com>
2022-06-13 00:46:31 -0300

    refactor(Context):Add Auth context

--- App.tsx
+++ App.tsx
@@ -3,10 +3,9 @@ import "intl";
 import "intl/locale-data/jsonp/pt-BR";
 
 import React from "react";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components";
-
-import { NavigationContainer } from "@react-navigation/native";
 
 import {
   useFonts,
@@ -17,9 +16,18 @@ import {
 
 import theme from "./src/global/styles/theme";
 
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+
 import { AppRoutes } from "./src/routes/app.routes";
 
+import { SignIn } from "./src/screens/SignIn";
+
+import { AuthProvider } from "./src/hooks/auth";
+
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
@@ -30,12 +38,18 @@ export default function App() {
     return null;
   }
 
+  SplashScreen.hideAsync();
+
   return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <StatusBar style="light" />
-        <AppRoutes />
-      </NavigationContainer>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <StatusBar style="light" />
+          <AuthProvider>
+            <SignIn />
+          </AuthProvider>
+        </NavigationContainer>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
