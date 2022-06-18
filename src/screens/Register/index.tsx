35fa3b9f216351e45cf8a2a055a3ef5e48593eef
fsanntiago <zerofabricio@hotmail.com>
2022-06-17 21:21:09 -0300

    refactor: Link user transactions

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -5,6 +5,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import uuid from "react-native-uuid";
 
+import { useAuth } from "../../hooks/auth";
 import { useForm } from "react-hook-form";
 import {
   NavigationProp,
@@ -49,6 +50,8 @@ export function Register() {
 
   const [transactionType, setTransactionType] = useState("");
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+  const { user } = useAuth();
 
   const [category, setCategory] = useState({
     key: "category",
@@ -106,8 +109,7 @@ export function Register() {
     };
 
     try {
-      const dataKey = "@gofinances:transactions";
-
+      const dataKey = `@gofinances:transactions_user:${user.id}`;
       const data = await AsyncStorage.getItem(dataKey);
       const currentData = data ? JSON.parse(data) : [];
 
